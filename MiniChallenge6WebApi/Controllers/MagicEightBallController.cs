252194079b73e156daa2bf26b563589b2f46eb56
dherrera-code <dherr929@gmail.com>
2026-01-13 13:27:12 -0800

    Initializing repository for mini challenge 6 with the magic eight ball endpoint functional!

---
+++ MiniChallenge6WebApi/Controllers/MagicEightBallController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MiniChallenge6WebApi.Services;
+
+namespace MiniChallenge6WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class MagicEightBallController : ControllerBase
+    {
+    private readonly Magic8BallService _content;
+
+        public MagicEightBallController(Magic8BallService content)
+    {
+        _content = content;
+    }
+
+        [HttpPost("/{askAQuestion}")]
+        public string Magic8Ball()
+        {
+            return _content.Magic8Ball();
+        }
+    }
+}
---
+++ MiniChallenge6WebApi/Services/Magic8BallService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniChallenge6WebApi.Services
+{
+    public class Magic8BallService
+    {
+        
+        public string Magic8Ball()
+        {
+            int randomNum = Random.Shared.Next(1, 11);
+            switch (randomNum)
+                {
+                        case 1:
+                            return "Ask again later";
+                        case 2:
+                            return "I don't think so buddy!";
+                        case 3:
+                            return "Outlook not so good. :( ";
+                        case 4:
+                            return "Ehh No!";
+                        case 5:
+                            return "It is Not Likely Try again!";
+                        case 6:
+                            return "It is Very Likely!";
+                        case 7:
+                            return "Don't count on it.";
+                        case 8:
+                            return "Yes, definitely!";
+                        case 9:
+                            return "Without a doubt!";
+                        default:
+                            return "Ask again later";
+                    }
+}
+
+    }
+}
