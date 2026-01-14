930ccd7f97bb9227aee640e3b0b204407dd2551e
dherrera-code <dherr929@gmail.com>
2026-01-14 13:12:28 -0800

    Implemented input validation towards guessIt Service!

--- MiniChallenge6WebApi/Services/GuessItService.cs
+++ MiniChallenge6WebApi/Services/GuessItService.cs
@@ -9,6 +9,11 @@ namespace MiniChallenge6WebApi.Services
     {
         public string StartGameEasy(int guess)
         {
+
+            if(guess > 10 || guess < 0)
+            {
+                return $"Your guess is outside the bounds of the guessing range! In easy mode, the range is from 1 to 10!";
+            }
             int randomNum = Random.Shared.Next(1,11);
            if(guess > randomNum)
             {
@@ -23,6 +28,11 @@ namespace MiniChallenge6WebApi.Services
         }
         public string StartGameMedium(int guess)
         {
+
+            if(guess > 50 || guess < 0)
+            {
+                return $"Your guess is outside the bounds of the guessing range! In easy mode, the range is from 1 to 50!";
+            }
             int randomNum = Random.Shared.Next(1,51);
            if(guess > randomNum)
             {
@@ -37,6 +47,10 @@ namespace MiniChallenge6WebApi.Services
         }
         public string StartGameHard(int guess)
         {
+            if(guess > 100 || guess < 0)
+            {
+                return $"Your guess is outside the bounds of the guessing range! In easy mode, the range is from 1 to 100!";
+            }
             int randomNum = Random.Shared.Next(1,101);
            if(guess > randomNum)
             {
